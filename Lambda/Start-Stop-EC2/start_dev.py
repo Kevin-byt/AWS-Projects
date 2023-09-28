50a82f0ed2b6f35169bbddd4169e93517da2694d
unknown <myapps255@gmail.com>
2023-09-28 14:52:32 +0300

    start stop with lambda

---
+++ Lambda/Start-Stop-EC2/start_dev.py
@@ -0,0 +1,66 @@
+# Start the instances in the dev environment:
+
+import boto3
+
+# AWS region and tag details
+aws_region = 'us-east-1'
+tag_key = 'env'
+tag_value = 'dev'
+
+# Create an EC2 client
+ec2 = boto3.client('ec2', region_name=aws_region)
+
+def lambda_handler(event, context):
+    # Get instances with the specified tag and value
+    instances_to_start = get_instances_with_tag(tag_key, tag_value)
+    print('intances')
+    print(instances_to_start)
+    
+    if instances_to_start:
+        # Start the instances
+        start_instances(instances_to_start)
+        return {
+            'statusCode': 200,
+            'body': 'Started instances: {}'.format(instances_to_start)
+        }
+    else:
+        print('No instances')
+        return {
+            'statusCode': 200,
+            'body': 'No instances found with tag "{}" and value "{}".'.format(tag_key, tag_value)
+        }
+
+def get_instances_with_tag(key, value):
+    try:
+        response = ec2.describe_instances(
+            Filters=[
+                {
+                    'Name': 'tag:' + key,
+                    'Values': [value]
+                }
+            ]
+        )
+        
+        print('Response is:')
+        print(response)
+        print('response done')
+        
+        instances = []
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                instances.append(instance['InstanceId'])
+        
+        print(instances)
+        return instances
+        
+    except Exception as e:
+        print('Error:', e)
+        return []
+
+ 
+def start_instances(instance_ids):
+    try:
+        ec2.start_instances(InstanceIds=instance_ids)
+        print('Starting instances:', instance_ids)
+    except Exception as e:
+        print('Error:', e)
---
+++ Lambda/Start-Stop-EC2/stop_dev.py
@@ -0,0 +1,58 @@
+#Stop instances in the dev environment
+
+import boto3
+
+# AWS region and tag details
+aws_region = 'us-east-1'
+tag_key = 'env'
+tag_value = 'dev'
+
+# Create an EC2 client
+ec2 = boto3.client('ec2', region_name=aws_region)
+
+def lambda_handler(event, context):
+    # Get instances with the specified tag and value
+    instances_to_stop = get_instances_with_tag(tag_key, tag_value)
+    
+    if instances_to_stop:
+        # Stop the instances
+        stop_instances(instances_to_stop)
+        return {
+            'statusCode': 200,
+            'body': 'Stopped instances: {}'.format(instances_to_stop)
+        }
+    else:
+        print('No instances')
+        return {
+            'statusCode': 200,
+            'body': 'No instances found with tag "{}" and value "{}".'.format(tag_key, tag_value)
+        }
+
+def get_instances_with_tag(key, value):
+    try:
+        response = ec2.describe_instances(
+            Filters=[
+                {
+                    'Name': 'tag:' + key,
+                    'Values': [value]
+                }
+            ]
+        )
+        
+        instances = []
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                instances.append(instance['InstanceId'])
+        
+        return instances
+        
+    except Exception as e:
+        print('Error:', e)
+        return []
+
+def stop_instances(instance_ids):
+    try:
+        ec2.stop_instances(InstanceIds=instance_ids)
+        print('Stopping instances:', instance_ids)
+    except Exception as e:
+        print('Error:', e)
