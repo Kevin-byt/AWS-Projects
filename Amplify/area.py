609b28e8e09ecd46d78ca8eeb2b1bcf734e23c6d
unknown <myapps255@gmail.com>
2023-10-13 00:53:29 +0300

    Amplify Web-App

--- Amplify/area.py
+++ Amplify/area.py
@@ -1,17 +1,18 @@
 # import the JSON utility package
 import json
-# import the Python math library
-import math
 
 # import the AWS SDK (for Python the package name is boto3)
 import boto3
+
 # import two packages to help us with dates and date formatting
 from time import gmtime, strftime
 
 # create a DynamoDB object using the AWS SDK
 dynamodb = boto3.resource('dynamodb')
+
 # use the DynamoDB object to select our table
-table = dynamodb.Table('PowerOfMathDatabase')
+table = dynamodb.Table('Area_table')
+
 # store the current time in a human readable format in a variable
 now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
@@ -19,17 +20,17 @@ now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 def lambda_handler(event, context):
 
 # extract the two numbers from the Lambda service's event object
-    mathResult = math.pow(int(event['base']), int(event['exponent']))
+    Area = int(event['length']) * int(event['width'])
 
 # write result and time to the DynamoDB table using the object we instantiated and save response in a variable
     response = table.put_item(
         Item={
-            'ID': str(mathResult),
+            'ID': str(Area),
             'LatestGreetingTime':now
             })
 
 # return a properly formatted JSON object
     return {
     'statusCode': 200,
-    'body': json.dumps('Your result is ' + str(mathResult))
+    'body': json.dumps('Your result is ' + str(Area))
     }
